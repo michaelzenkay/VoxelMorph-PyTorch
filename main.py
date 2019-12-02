978f73eb3d7aacc2a44614c2c22698ee0f2fe2b5
michaelzenkay <michaelzenkay@gmail.com>
2019-12-01 19:41:16 -0500

    works on gpu collapsed voxelmorph2d and 3d into single file get rid of use_gpu flag
    Signed-off-by: michaelzenkay <michaelzenkay@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import voxelmorph2d as vm2d
-import voxelmorph3d as vm3d
+import network
 import torch
-import torchvision
+# import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
@@ -26,23 +25,24 @@ class VoxelMorph():
     VoxelMorph Class is a higher level interface for both 2D and 3D
     Voxelmorph classes. It makes training easier and is scalable.
     """
-
-    def __init__(self, input_dims, is_2d=False, use_gpu=False):
+    def __init__(self, input_dims, dim):
         self.dims = input_dims
-        if is_2d:
-            self.vm = vm2d
-            self.voxelmorph = vm2d.VoxelMorph2d(input_dims[0] * 2, use_gpu)
-        else:
-            self.vm = vm3d
-            self.voxelmorph = vm3d.VoxelMorph3d(input_dims[0] * 2, use_gpu)
-        self.optimizer = optim.SGD(
-            self.voxelmorph.parameters(), lr=1e-4, momentum=0.99)
+        self.dim  = dim
+        self.net = self.load_model()
+        self.optimizer = optim.SGD(self.net.parameters(), lr=1e-4, momentum=0.99)
         self.params = {'batch_size': 3,
                        'shuffle': True,
                        'num_workers': 6,
                        'worker_init_fn': np.random.seed(42)
                        }
-        self.device = torch.device("cuda:0" if use_gpu else "cpu")
+        self.criteria = self.cc_smooth
+
+    def load_model(self):
+        in_channel = self.dims[0] * 2
+        self.net = network.VM_Net(in_channel, dim=self.dim)
+        torch.cuda.set_device('cuda:0')
+        self.net.cuda()
+        return self.net
 
     def check_dims(self, x):
         try:
@@ -56,34 +56,106 @@ class VoxelMorph():
 
     def forward(self, x):
         self.check_dims(x)
-        return voxelmorph(x)
 
-    def calculate_loss(self, y, ytrue, n=9, lamda=0.01, is_training=True):
-        loss = self.vm.vox_morph_loss(y, ytrue, n, lamda)
+###### LOSSES
+    def cross_correlation(self, I, J, n):
+        I = I.permute(0, 3, 1, 2).cuda()
+        J = J.permute(0, 3, 1, 2).cuda()
+        batch_size, channels, xdim, ydim = I.shape
+        I2 = torch.mul(I, I).cuda()
+        J2 = torch.mul(J, J).cuda()
+        IJ = torch.mul(I, J).cuda()
+        sum_filter = torch.ones((1, channels, n, n)).cuda()
+
+        I_sum = torch.conv2d(I, sum_filter, padding=1, stride=(1, 1))
+        J_sum = torch.conv2d(J, sum_filter, padding=1, stride=(1, 1))
+
+        I2_sum = torch.conv2d(I2, sum_filter, padding=1, stride=(1, 1))
+        J2_sum = torch.conv2d(J2, sum_filter, padding=1, stride=(1, 1))
+        IJ_sum = torch.conv2d(IJ, sum_filter, padding=1, stride=(1, 1))
+
+        win_size = n ** 2
+        u_I = I_sum / win_size
+        u_J = J_sum / win_size
+
+        cross = IJ_sum - u_J * I_sum - u_I * J_sum + u_I * u_J * win_size
+
+        I_var = I2_sum - 2 * u_I * I_sum + u_I * u_I * win_size
+        J_var = J2_sum - 2 * u_J * J_sum + u_J * u_J * win_size
+
+        cc = cross * cross / (I_var * J_var + np.finfo(float).eps)
+
+        return torch.mean(cc)
+
+    def smooothing(self, y_pred):
+        dy = torch.abs(y_pred[:, 1:, :, :] - y_pred[:, :-1, :, :])
+        dx = torch.abs(y_pred[:, :, 1:, :] - y_pred[:, :, :-1, :])
+
+        dx = torch.mul(dx, dx)
+        dy = torch.mul(dy, dy)
+        d = torch.mean(dx) + torch.mean(dy)
+        return d / 2.0
+
+    def cc_smooth(self, y, ytrue, n=9, lamda=0.01):
+        cc = self.cross_correlation(y, ytrue, n)
+        sm = self.smooothing(y)
+        # print("CC Loss", cc, "Gradient Loss", sm)
+        loss = -1.0 * cc + lamda * sm
         return loss
 
-    def train_model(self, batch_moving, batch_fixed, n=9, lamda=0.01, return_metric_score=True):
+    def dice(self,pred, target):
+        """This definition generalize to real valued pred and target vector. This should be differentiable.
+        pred: tensor with first dimension as batch
+        target: tensor with first dimension as batch
+        """
+        top = 2 * torch.sum(pred * target, [1, 2, 3])
+        union = torch.sum(pred + target, [1, 2, 3])
+        eps = torch.ones_like(union) * 1e-5
+        bottom = torch.max(union, eps)
+        dice = torch.mean(top / bottom)
+        # print("Dice score", dice)
+        return dice
+
+######## TRAIN MODEL
+    def train_model(self, batch_moving, batch_fixed, n=9, lamda=0.01, calc_dice=False):
+        # Reset Gradients
         self.optimizer.zero_grad()
-        batch_fixed, batch_moving = batch_fixed.to(
-            self.device), batch_moving.to(self.device)
-        registered_image = self.voxelmorph(batch_moving, batch_fixed)
-        train_loss = self.calculate_loss(
-            registered_image, batch_fixed, n, lamda)
+
+        # Move images to gpu
+        batch_fixed.cuda()
+        batch_moving.cuda()
+
+        # Forward Pass
+        batch_registered = self.net(batch_moving, batch_fixed)
+
+        # Calculate Loss
+        train_loss = self.criteria(batch_registered, batch_fixed, n, lamda)
+
+        # Have to figure out why batch_fixed pops off gpu -> cpu ?
+
+        # Backward Pass
         train_loss.backward()
+
+        # Step
         self.optimizer.step()
-        if return_metric_score:
-            train_dice_score = self.vm.dice_score(
-                registered_image, batch_fixed)
-            return train_loss, train_dice_score
+
+        # Return metrics
+        if calc_dice:
+            train_dice = self.dice(batch_registered, batch_fixed.cuda())
+            return train_loss, train_dice
         return train_loss
 
-    def get_test_loss(self, batch_moving, batch_fixed, n=9, lamda=0.01):
+######## Calculate Losses
+    def get_test_loss(self, batch_moving, batch_fixed, n=9, lamda=0.01, calc_dice=False):
         with torch.set_grad_enabled(False):
-            registered_image = self.voxelmorph(batch_moving, batch_fixed)
-            val_loss = self.vm.vox_morph_loss(
-                registered_image, batch_fixed, n, lamda)
-            val_dice_score = self.vm.dice_score(registered_image, batch_fixed)
-            return val_loss, val_dice_score
+            batch_moving.cuda()
+            batch_fixed.cuda()
+            batch_registered = self.net(batch_moving, batch_fixed)
+            val_loss = self.criteria(batch_registered, batch_fixed, n, lamda)
+            if calc_dice:
+                val_dice_score = self.dice(batch_registered, batch_fixed.cuda())
+                return val_loss, val_dice_score
+            return val_loss
 
 
 class Dataset(data.Dataset):
@@ -120,16 +192,15 @@ def main():
     In this I'll take example of FIRE: Fundus Image Registration Dataset
     to demostrate the working of the API.
     '''
-    vm = VoxelMorph(
-        (3, 256, 256), is_2d=True)  # Object of the higher level class
+    vm = VoxelMorph((3, 256, 256), dim=2)  # Object of the higher level class
     DATA_PATH = './fire-fundus-image-registration-dataset/'
-    params = {'batch_size': 1,
+    params = {'batch_size': 16,
               'shuffle': True,
               'num_workers': 6,
               'worker_init_fn': np.random.seed(42)
               }
 
-    max_epochs = 2
+    max_epochs = 5
     filename = list(set([x.split('_')[0]
                          for x in os.listdir('./fire-fundus-image-registration-dataset/')]))
     partition = {}
@@ -151,20 +222,17 @@ def main():
         val_loss = 0
         val_dice_score = 0
         for batch_fixed, batch_moving in training_generator:
-            loss, dice = vm.train_model(batch_moving, batch_fixed)
-            train_dice_score += dice.data
+            loss = vm.train_model(batch_moving, batch_fixed)
             train_loss += loss.data
         print('[', "{0:.2f}".format((time.time() - start_time) / 60), 'mins]', 'After', epoch + 1, 'epochs, the Average training loss is ', train_loss *
-              params['batch_size'] / len(training_set), 'and average DICE score is', train_dice_score.data * params['batch_size'] / len(training_set))
+              params['batch_size'] / len(training_set))
         # Testing time
         start_time = time.time()
         for batch_fixed, batch_moving in validation_generator:
-            # Transfer to GPU
-            loss, dice = vm.get_test_loss(batch_moving, batch_fixed)
-            val_dice_score += dice.data
+            loss = vm.get_test_loss(batch_moving, batch_fixed)
             val_loss += loss.data
         print('[', "{0:.2f}".format((time.time() - start_time) / 60), 'mins]', 'After', epoch + 1, 'epochs, the Average validations loss is ', val_loss *
-              params['batch_size'] / len(validation_set), 'and average DICE score is', val_dice_score.data * params['batch_size'] / len(validation_set))
+              params['batch_size'] / len(validation_set))
 
 
 if __name__ == "__main__":
